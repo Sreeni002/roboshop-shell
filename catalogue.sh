bf26a1bd749648c5deb44d9e8657ffb2f364873c
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-18 10:11:58 +0530

    Adding comments in the code for debugging

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,37 @@
+echo -e "\e[36m>>>>>>>>>>>Configuring nodejs repos<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>>>>>Install nodejs <<<<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>>>>Create app user <<<<<<<<<<<\e[0m"
 useradd roboshop
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>>>Download app content<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>>Unzip app content<<<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>>Install nodeJS dependencies<<<<<<<<<<<\e[0m"
 npm install
+
+echo -e "\e[36m>>>>>>>>>>>Copy catalogue service<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>>>>>>>start catalogue service<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 
+echo -e "\e[36m>>>>>>>>>>>Copy mongo repo files<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>>>>Install mongo client<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>>>>>>>Load mongo schema<<<<<<<<<<<\e[0m"
 mongo --host mongodb.sreenivasulareddydevops.online </app/schema/catalogue.js
