5eb5436afc3236ccbe266294d4329bfcffbab0c0
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-18 16:32:52 +0530

    Update coments in shipping service

--- shipping.sh
+++ shipping.sh
@@ -1,16 +1,32 @@
+echo -e "\e[36m>>>>>>>>>>>Install Maven<<<<<<<<<<<\e[0m"
 yum install maven -y
+
+echo -e "\e[36m>>>>>>>>>>>Add application user<<<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>>>>Create application directory<<<<<<<<<<<\e[0m"
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>>>>Download the app content<<<<<<<<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>>CExtract the app content<<<<<<<<<<<\e[0m"
 unzip /tmp/shipping.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>>>>Download Maven dependencies<<<<<<<<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+
+echo -e "\e[36m>>>>>>>>>>>Install Mysql<<<<<<<<<<<\e[0m"
+yum install mysql -y
+
+echo -e "\e[36m>>>>>>>>>>>Load schema<<<<<<<<<<<\e[0m"
+mysql -h mysql.sreenivasulareddydevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>>>>>>>>Start shipping service<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
-
-yum install mysql -y
-mysql -h mysql.sreenivasulareddydevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping
+
