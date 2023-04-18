ce6e1e01405b9ecf1f277692adc3576fc2510aeb
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-18 16:54:44 +0530

    Update coments in rabbitmq service

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,8 +1,18 @@
+echo -e "\e[36m>>>>>>>>>>>Setup Earlng Repos<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-yum install erlang -y
+
+echo -e "\e[36m>>>>>>>>>>>Setup rabbitmq repos<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-yum install rabbitmq-server -y
+
+echo -e "\e[36m>>>>>>>>>>>Install rabbitmq<<<<<<<<<<<\e[0m"
+yum install erlang rabbitmq-server -y
+
+echo -e "\e[36m>>>>>>>>>>>Start rabbitmq service<<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
+
+echo -e "\e[36m>>>>>>>>>>>Add application user in rabbitmq and setup password <<<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
+
+echo -e "\e[36m>>>>>>>>>>>Grant the permissions to the application user<<<<<<<<<<<\e[0m"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
