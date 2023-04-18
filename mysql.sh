951a831325c3129ec8037a4f07236934cf6b847c
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-18 16:16:30 +0530

    Update coments in mysql service

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo -e "\e[36m>>>>>>>>>>>Disable mysql 8 version<<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>>>>Copy my sql repo file <<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[36m>>>>>>>>>>>Install my sql <<<<<<<<<<<\e[0m"
 yum install mysql-community-server -y
+
+echo -e "\e[36m>>>>>>>>>>>Start my sql <<<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo -e "\e[36m>>>>>>>>>>>Reset my sql password <<<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
