e4c1db173166f7fc3e88876931029e6746d2ffa0
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-19 19:42:33 +0530

    Upated the path in all the shell files using the variable

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,4 +1,5 @@
-script_path=$(dirname $0)
+script=$(realpath "$0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>>>>>Setup Earlng Repos<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
