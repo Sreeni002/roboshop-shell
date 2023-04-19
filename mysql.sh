8216c281823bf059b6c078ca6fea75ee22900e7d
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-19 21:07:08 +0530

    Upated the path in all the shell files using the variable

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,11 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+if [ -z "$mysql_root_password" ]; then
+    echo Input My SQL root password is missing
+    exit
+fi
+
 echo -e "\e[36m>>>>>>>>>>>Disable mysql 8 version<<<<<<<<<<<\e[0m"
 dnf module disable mysql -y
 
@@ -16,4 +21,4 @@ systemctl enable mysqld
 systemctl start mysqld
 
 echo -e "\e[36m>>>>>>>>>>>Reset my sql password <<<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass $mysql_root_password
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,6 +2,12 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_appuser_password=$1
+
+if [ -z "$rabbitmq_appuser_password"]; then
+    echo input Roboshop app user password missing
+    exit
+fi
+
 echo -e "\e[36m>>>>>>>>>>>Setup Earlng Repos<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
