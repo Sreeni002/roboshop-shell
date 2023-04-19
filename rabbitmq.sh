4b0577bb750df3ed4e0c0a04cf3f86fd22345def
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-19 20:07:35 +0530

    Upated the path in all the shell files using the variable

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 echo -e "\e[36m>>>>>>>>>>>Setup Earlng Repos<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
@@ -15,7 +16,7 @@ systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
 
 echo -e "\e[36m>>>>>>>>>>>Add application user in rabbitmq and setup password <<<<<<<<<<<\e[0m"
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop ${rabbitmq_appuser_password}
 
 echo -e "\e[36m>>>>>>>>>>>Grant the permissions to the application user<<<<<<<<<<<\e[0m"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
+
 echo -e "\e[36m>>>>>>>>>>>Install Maven<<<<<<<<<<<\e[0m"
 yum install maven -y
 
@@ -26,7 +28,7 @@ echo -e "\e[36m>>>>>>>>>>>Install Mysql<<<<<<<<<<<\e[0m"
 yum install mysql -y
 
 echo -e "\e[36m>>>>>>>>>>>Load schema<<<<<<<<<<<\e[0m"
-mysql -h mysql-dev.sreenivasulareddydevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.sreenivasulareddydevops.online -uroot -${mysql_root_password} < /app/schema/shipping.sql
 
 echo -e "\e[36m>>>>>>>>>>>setup systemD service file <<<<<<<<<<<\e[0m"
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
