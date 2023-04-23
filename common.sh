42df5f17d41d60cded2089d57817602497cdc290
Sreeni.B <sreenu.reddy002@gmail.com>
2023-04-23 19:10:18 +0530

    Applied more function to reduce the DRY

--- common.sh
+++ common.sh
@@ -1,54 +1,110 @@
 app_user=roboshop
 
-print_head() {
+Func_print_head() {
 echo -e "\e[36m>>>>>>>>>>>$1<<<<<<<<<<<\e[0m"
 }
 
+func_status_check() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUSSESS\e[0m"
+  else
+    echo -e "\e[31mFailure\e[0m"
+    exit 1
+  fi
+}
 schema_setup(){
 if [ "schema_setup" == "mongo" ]; then
-echo -e "\e[36m>>>>>>>>>>>Copy mongo repo file<<<<<<<<<<<\e[0m"
+Func_print_head "Copy mongo repo file"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>>>Install mongo client<<<<<<<<<<<\e[0m"
+Func_print_head"Install mongo client"
 yum install mongodb-org-shell -y
+func_status_check $?
 
-echo -e "\e[36m>>>>>>>>>>>Load schema<<<<<<<<<<<\e[0m"
+Func_print_head"Load schema"
 mongo --host mongodb-dev.sreenivasulareddydevops.online </app/schema/${component}.js
+func_status_check $?
+fi
+if [ "${schema_setup}" == "mysql" ]; then
+
+Func_print_head "Install Mysql client"
+yum install mysql -y
+func_status_check $?
+
+Func_print_head "Load schema"
+mysql -h mysql-dev.sreenivasulareddydevops.online -uroot -${mysql_root_password} < /app/schema/shipping.sql
+func_status_check $?
 fi
 }
 
-func_nodeJs()
-{
-print_head "Configuring nodejs repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+func_app_prereq(){
+Func_print_head "Create application user"
+useradd roboshop
 
-print_head "Install nodejs"
-yum install nodejs -y
-
-print_head "Create app user"
-useradd ${app_user}
-
-print_head "Create app directory"
+Func_print_head "Create application directory"
 rm -rf /app
 mkdir /app
+func_status_check $?
 
-print_head "Download app content"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+
+Func_print_head "Download the app content"
+curl -L -o /tmp/${componet}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 cd /app
+func_status_check $?
 
-print_head "Unzip app content"
+Func_print_head "Extract the app content"
 unzip /tmp/${component}.zip
-cd /app
+func_status_check $?
 
-print_head "Install nodeJS dependencies"
-npm install
+func_status_check $?
+}
 
-print_head "Copy cart service"
+func_system_setup(){
+Func_print_head "Setup systemd service"
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+func_status_check $?
 
-print_head "Start cart service"
+Func_print_head "Start cart service"
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl start ${component}
+func_status_check $?
 schema_setup
+func_status_check $?
+}
+func_nodeJs()
+{
+Func_print_head "Configuring nodejs repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+func_status_check $?
+
+Func_print_head "Install nodejs"
+yum install nodejs -y
+func_status_check $?
+
+func_app_prereq
+
+func_print_head "Install nodeJS dependencies"
+npm install
+func_status_check $?
+
+func_schema_setup
+func_system_setup
+func_status_check $?
+}
+
+func_java(){
+
+Func_print_head "Install Maven"
+yum install maven -y
+func_status_check $?
+
+func_app_prereq
+Func_print_head "Download Maven dependencies"
+mvn clean package
+func_status_check $?
+mv target/${component}-1.0.jar ${component}.jar
+func_status_check $?
+func_system_setup()
 }
--- shipping.sh
+++ shipping.sh
@@ -8,38 +8,6 @@ if [ -z "mysql_root_password" ]; then
     exit
 fi
 
-echo -e "\e[36m>>>>>>>>>>>Install Maven<<<<<<<<<<<\e[0m"
-yum install maven -y
-
-echo -e "\e[36m>>>>>>>>>>>Add application user<<<<<<<<<<<\e[0m"
-useradd roboshop
-
-echo -e "\e[36m>>>>>>>>>>>Create application directory<<<<<<<<<<<\e[0m"
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>>>>Download the app content<<<<<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-
-echo -e "\e[36m>>>>>>>>>>>Extract the app content<<<<<<<<<<<\e[0m"
-unzip /tmp/shipping.zip
-cd /app
-
-echo -e "\e[36m>>>>>>>>>>>Download Maven dependencies<<<<<<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>>>>>>>Install Mysql<<<<<<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>>>>>>Load schema<<<<<<<<<<<\e[0m"
-mysql -h mysql-dev.sreenivasulareddydevops.online -uroot -${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>>>>>>>>setup systemD service file <<<<<<<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>>>>>>Start shipping service<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
-
+component="shipping"
+schema_setup="mysql"
+func_java(){
